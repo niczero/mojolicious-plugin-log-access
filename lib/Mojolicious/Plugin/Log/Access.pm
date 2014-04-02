9a0c96b9f9d053fb70036de5f9ec6b6a3e5c2a45
Nic Sandfield <niczero@cpan.org>
2014-04-02 20:47:34 +0100

    Improved documentation

--- lib/Mojolicious/Plugin/Log/Access.pm
+++ lib/Mojolicious/Plugin/Log/Access.pm
@@ -1,7 +1,7 @@
 package Mojolicious::Plugin::Log::Access;
 use Mojo::Base 'Mojolicious::Plugin';
 
-our $VERSION = 0.011;
+our $VERSION = 0.012;
 
 sub register {
   my ($self, $app) = @_;
@@ -22,15 +22,31 @@ Mojolicious::Plugin::Log::Access - Provide access logging
 =head1 SYNOPSIS
 
   # Mojolicious
-  $self->plugin('Mojolicious::Plugin::Log::Access');
+  $self->plugin('Log::Access');
 
   # Mojolicious::Lite
-  plugin 'Mojolicious::Plugin::Log::Access';
+  plugin 'Log::Access';
 
 =head1 DESCRIPTION
 
 L<Mojolicious::Plugin::Log::Access> is a L<Mojolicious> plugin for adding access
 logging to your web application.
+
+=head1 USAGE
+
+Simply add the plugin as shown above.  Whenever the app is in log levels 'debug'
+or 'info', it will log each page access as an 'info' message.
+
+Alternatively, if you only want info-level access logging when in 'production'
+mode, you can restrict the plugin for only that mode.
+
+  sub startup {
+    my $self = shift;
+    $self->plugin('Log::Access') if $self->mode eq 'production';
+
+Currently the log entry format is hard-coded.
+
+  [info] <ip address> <page> <status code>
 
 =head1 METHODS
 
@@ -42,6 +58,29 @@ L<Mojolicious::Plugin>.
   $plugin->register(Mojolicious->new);
 
 Register plugin in L<Mojolicious> application.
+
+=head1 RATIONALE
+
+It is fairly typical that in a production environment web apps will run in
+'production' mode and use a log level of 'info'.  Often the administrators will
+expect page hits to be logged in a similar style to the likes of apache and
+nginx, showing ip address and resulting status code.
+
+This solves an unrelated problem for me.  Sometimes when migrating legacy css or
+js, it is important to monitor and catch all 404s; these indicate migration work
+yet unfinished.  It can be difficult to see them at log level 'debug'.
+
+Sebastian suggested using an after_dispatch hook in a plugin, so here it is.
+When needing to solve the second problem I can develop in log level 'info' since
+production-style access logging is exactly what I need.
+
+=head1 COPYRIGHT AND LICENSE
+
+Everything here (incl tools and ideas) was provided by Sebastian Riedel.  It was
+packaged by Nic Sandfield.
+
+This program is free software, you can redistribute it and/or modify it under
+the terms of the Artistic License version 2.0.
 
 =head1 SEE ALSO
 
